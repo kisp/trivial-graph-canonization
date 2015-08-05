7145ef61847aa3aea8e79e64aae82c67d8a30b45
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-08-05 20:47:58 +0200

    Optimized for speed.

--- trivial-graph-canonization.lisp
+++ trivial-graph-canonization.lisp
@@ -2,7 +2,7 @@
 
 (in-package :trivial-graph-canonization)
 
-(defun build-vector (graph order nodes)
+(defun build-matrix (graph order nodes)
   (let ((matrix (make-array (list order order) :element-type 'bit)))
     (iter
       (for a in nodes)
@@ -12,24 +12,63 @@
         (for j upfrom 0)
         (setf (aref matrix i j)
               (if (graph:has-edge-p graph (list a b)) 1 0))))
-    (make-array (* order order) :displaced-to matrix :element-type 'bit)))
+    matrix))
 
+(defun build-vector (matrix order nodes)
+  (declare (type (simple-array bit) matrix))
+  (declare (type positive-fixnum order))
+  (declare (type list nodes))
+  (declare (optimize speed (safety 0)))
+  (let ((vector (make-array (the fixnum (* order order))
+                            :element-type 'bit)))
+    (iter
+      (for a in nodes)
+      (for i upfrom 0)
+      (for i-order = (* i order))
+      (declare (fixnum i i-order))
+      (iter
+        (for b in nodes)
+        (for j upfrom 0)
+        (declare (fixnum j))
+        (setf (aref vector (+ i-order j))
+              (aref matrix a b))))
+    vector))
+
+(declaim (inline vector-lex-<))
 (defun vector-lex-< (a b)
+  (declare (type simple-bit-vector a b))
+  (declare (optimize speed (safety 0)))
   (iter
     (for i from 0 below (length a))
-    (when (< (aref a i) (aref b i))
+    (for a-i = (aref a i))
+    (for b-i = (aref b i))
+    (declare (fixnum i))
+    (when (< a-i b-i)
       (return t))
-    (when (> (aref a i) (aref b i))
+    (when (> a-i b-i)
       (return nil))))
+
+(defun find-minimum (list)
+  (declare (optimize speed (safety 0)))
+  (iter
+    (with result = (first list))
+    (for x in (rest list))
+    (when (vector-lex-< x result)
+      (setq result x))
+    (finally (return result))))
+
+(defun graph-canonization-not-zero (graph order nodes)
+  (let (list
+        (matrix (build-matrix graph order nodes)))
+    (map-permutations
+     (lambda (nodes)
+       (push (build-vector matrix order nodes) list))
+     (iota order) :copy nil)
+    (find-minimum list)))
 
 (defun graph-canonization (graph)
   (let* ((nodes (graph:nodes graph))
          (order (length nodes)))
     (if (zerop order)
         (make-array 0 :element-type 'bit)
-        (let (list)
-          (map-permutations
-           (lambda (nodes)
-             (push (build-vector graph order nodes) list))
-           nodes :copy nil)
-          (first (sort list #'vector-lex-<))))))
+        (graph-canonization-not-zero graph order nodes))))
